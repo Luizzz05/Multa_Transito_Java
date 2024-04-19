4eaed0627fba0c7ed21a4d086500b2f09798b2da
Luiz Felipe S Santos <lfsilvasantos55@gmail.com>
2024-04-19 10:04:11 -0300

    Finalização da atividade

--- src/funcao/Arquivo.java
+++ src/funcao/Arquivo.java
@@ -1,13 +1,10 @@
 package funcao;
 
-import java.awt.HeadlessException;
-import java.io.BufferedReader;
 import java.io.BufferedWriter;
-import java.io.FileReader;
+import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Scanner;
 
 import javax.swing.JOptionPane;
 
@@ -17,41 +14,26 @@ import model.Carro;
 public class Arquivo {
 	
 	static Carro c = new Carro();
-	List<Carro> list = new ArrayList<>();
-	public void readCarro(String arquivo) throws IOException {
-		String path = "/home/luiz/Documentos/eclipse-workspace/Atividade_Java/src/Temp/multas.csv" + arquivo;//Simplificado
-		
-		
-		try (BufferedReader br = new BufferedReader(new FileReader(path))){ //Simplificado
-			String line = br.readLine(); //Simplificado
-			line = br.readLine();
-			while(line != null) { //Simplificado
-//				JOptionPane.showMessageDialog(null, line); //Simplificado
-				
-				String[] vect = line.split(";");
-				String modelo = vect[0];
-				String placa = vect[1];
-				String ano = vect[2];
-				String cor = vect[3];
-				String velocidadeCarro = vect[4];
-				Double calcularMulta = Double.parseDouble(vect[5]);
-				
-				Carro prod = new Carro(modelo, placa, ano, cor, velocidadeCarro);
-				
-				list.add(prod);
-				
-				line = br.readLine(); //Simplificado
-				
-			}
-			System.out.println("Informação da multa");
-			for(Carro c :list) {
-				JOptionPane.showMessageDialog(null, c);
-			}
-			
-		}
-	}
-	
-	public void writeCarro(String arquivo, int multa) throws IOException{
+    public void readCarro(String arquivo) {
+        File path = new File("/home/luiz/Documentos/eclipse-workspace/Atividade_Java/src/Temp/" + arquivo);
+        Scanner read = null;
+        try {
+            read = new Scanner(path);
+            StringBuilder conteudo = new StringBuilder();
+            while (read.hasNextLine()) {
+                conteudo.append(read.nextLine()).append("\n"); // Adiciona cada linha ao StringBuilder
+                
+            }
+            JOptionPane.showMessageDialog(null, conteudo.toString(), "Relatório das Multas", JOptionPane.INFORMATION_MESSAGE); // Exibe o conteúdo completo do arquivo
+        } catch (IOException e) {
+            JOptionPane.showMessageDialog(null, e.getMessage());
+        } finally {
+            if (read != null) {
+                read.close();
+            }
+        }
+    }
+	public void writeCarro(String arquivo, double multa) throws IOException{
 		String path = "/home/luiz/Documentos/eclipse-workspace/Atividade_Java/src/Temp/" + arquivo;
 		
 		try {
@@ -64,18 +46,29 @@ public class Arquivo {
 			c.setCor(JOptionPane.showInputDialog("Digite a cor do carro: "));
 			c.setVelocidadeCarro(JOptionPane.showInputDialog("Digite a velocidade do carro: "));
 
+	        int velocidadeVia = 80; // velocidade máxima permitida
+	        int velocidadeCarro = Integer.parseInt(c.getVelocidadeCarro());
+	        int velocidadeExcedida = velocidadeCarro - velocidadeVia;
+
+
+	        if (velocidadeExcedida > 0) {
+	            multa = calcularMulta(velocidadeExcedida, velocidadeVia);
+	        }
 			
 			bw.write(c.getModelo());
-			bw.write(";");
+			bw.write("; ");
 			bw.write(c.getPlaca());
-			bw.write(";");
+			bw.write("; ");
 			bw.write(c.getAno());
-			bw.write(";");
+			bw.write("; ");
 			bw.write(c.getCor());
-			bw.write(";");
+			bw.write("; ");
 			bw.write(c.getVelocidadeCarro());
+			bw.write("; ");
+			bw.write(String.valueOf(multa)); // Escreve o valor da multa no arquivo
+            bw.write("\n");
 			
-			bw.write("\n");
+			
 			bw.close();
 						
 		} catch (IOException e) {
@@ -83,20 +76,21 @@ public class Arquivo {
 		}
 	}
 	
-	public void printcarro() {
+	public void printCarro() {
 		
-		JOptionPane.showMessageDialog(null, "Os dados co carro são: Modelo: "+ c.getModelo()
-																	+ " Placa: " + c.getPlaca()
-																	+ " Ano: "+ c.getAno()
-																	+ " Cor: " + c.getCor()
-																	+ " Velocidade: " + c.getVelocidadeCarro());
+		JOptionPane.showMessageDialog(null, "Modelo: "+ c.getModelo()
+											+ "\nPlaca: " + c.getPlaca()
+											+ "\nAno: "+ c.getAno()
+											+ "\nCor: " + c.getCor()
+											+ "\nVelocidade: " + c.getVelocidadeCarro(),
+											"As informações do carro são: ", JOptionPane.INFORMATION_MESSAGE);
 	}
 
 	public static void multaTransito(){
     	int velocidadeVia = 80;
         int velocidadeExcedida = Integer.parseInt(c.getVelocidadeCarro()) - velocidadeVia;
         if (velocidadeExcedida > 0) {
-            double multa = calcularMulta(velocidadeExcedida, velocidadeExcedida);
+            double multa = calcularMulta(velocidadeExcedida, velocidadeVia);
             JOptionPane.showMessageDialog(null, "Multa a ser paga: R$" + multa);
         } else {
         	JOptionPane.showMessageDialog(null, "O carro está dentro do limite de velocidade." + c.getModelo());
--- src/principal/Principal.java
+++ src/principal/Principal.java
@@ -4,26 +4,19 @@ import java.io.IOException;
 import javax.swing.JOptionPane;
 
 import funcao.Arquivo;
-import funcao.Funcao;
 import model.Carro;
 
 public class Principal {
 
-	public static void main(String[] args) throws  IOException {
-		String arquivo;
-        Carro carro = new Carro();
-        Arquivo arq =new Arquivo();
-      
-      
-        
-        arquivo = JOptionPane.showInputDialog("Digite o caminho e o nome do arquivo. exemplo: ''ini.txt'': ");
-        arq.writeCarro(arquivo);
-        arq.printcarro();
+	public static void main(String[] args) throws IOException {
+        String arquivo;
+        Arquivo arq = new Arquivo();
+
+        arquivo = JOptionPane.showInputDialog("Digite o caminho e o nome do arquivo, por exemplo: ''ini.txt'': ");
+        arq.writeCarro(arquivo, 0);
+        arq.printCarro();
         arq.multaTransito();
         arq.readCarro(arquivo);
-        System.out.println("");
     }
-
- 
 }
 
